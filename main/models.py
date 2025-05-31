e9a00a4ab30addd8b440aaa116c3381dbfbef3e8
aydin <aidinafsary@gmail.com>
2025-05-31 16:36:51 +0330

    config db

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Games(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    address = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Games')
+    price = models.IntegerField(default=60)
+
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Games
 
 # Create your views here.
 
 def main(request):
-    return render(request , "main/index.html")
+    games = Games.objects.all()
+    return render(request , "main/index.html" , context={'games': games})
